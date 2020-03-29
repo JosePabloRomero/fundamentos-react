cf040fbfed1a83d17f78744b27b8c9b2a8ad7cd3
Jose Pablo Romero <josepablo.developer@gmail.com>
2020-03-29 16:21:35 -0500

    Etiqueta select añadidad a input controlado

--- src/components/InputControlado/InputControlado.js
+++ src/components/InputControlado/InputControlado.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 const Alien = () => (
     <span role='img' aria-label='Alien'>
@@ -6,34 +6,40 @@ const Alien = () => (
     </span>
 )
 
-class InputControlado extends Component{
+const Zombie = () => (
+    <span role='img' aria-label='Zombie'>
+        🧟‍♂️
+    </span>
+)
+
+class InputControlado extends Component {
     state = {
-        text: '',        
+        text: '',
         color: '#E8E8E8'
     }
 
     actualizar = (event) => {
-        const text = event.target.value        
+        const text = event.target.value
         let color = 'green'
-        if (text.trim() === ''){
+        if (text.trim() === '') {
             color = '#E8E8E8'
         }
         if (text.trim() !== '' && text.trim().length < 5) {
             color = 'red'
         }
 
-        this.setState({ text,color })
+        this.setState({ text, color })
         this.props.onChange(this.props.name, text)
     }
-    render(){
+    render() {
         const styles = {
             border: `1px solid ${this.state.color}`,
             padding: '0.3em 0.6em',
             outline: 'none'
         }
-        return(
-            <input 
-                type="text" 
+        return (
+            <input
+                type="text"
                 value={this.state.text}
                 onChange={this.actualizar}
                 style={styles}
@@ -43,7 +49,33 @@ class InputControlado extends Component{
     }
 }
 
-class App extends Component{
+class Select extends Component {
+    state = {
+        tech: 'Vue'
+    }
+    handleChange = (event) => {
+        this.setState({
+            tech: event.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Etiqueta Select <Zombie/> {this.state.tech} </h1>
+                <form>
+                    <select value={this.state.tech} onChange={this.handleChange}>
+                        <option value="Angular">Angular</option>
+                        <option value="React">React</option>
+                        <option value="Vue">Vue</option>
+                        <option value="Vanilla">Vanilla</option>
+                    </select>
+                </form>
+            </div>
+        )
+    }
+}
+
+class App extends Component {
     state = {
         name: '',
         email: ''
@@ -53,18 +85,18 @@ class App extends Component{
             [name]: text
         })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h1>
-                    Inputs Controlados <Alien/>                    
+                    Inputs Controlados <Alien />
                 </h1>
-                <InputControlado 
+                <InputControlado
                     onChange={this.actualizar}
                     name='name'
                     placeholder='Nombre Completo'
                 />
-                <InputControlado 
+                <InputControlado
                     onChange={this.actualizar}
                     name='email'
                     placeholder='Tu Email'
@@ -75,6 +107,7 @@ class App extends Component{
                 <h2>
                     Email: {this.state.email}
                 </h2>
+                <Select></Select>
             </div>
         )
     }
