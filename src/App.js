0887e6fb8265c238f8dbea9d4097c19fdb148960
Jose Pablo Romero <josepablo.developer@gmail.com>
2020-03-29 17:20:04 -0500

    Etiqueta select con multiples opciones

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import InputNoControlado from './components/InputNoControlado'
 import InputNoControladoForm from './components/InputNoControladoForm'
 import InputControlado from './components/InputControlado'
+import InputSelectOption from './components/InputSelectOption'
 const App = () => (
     <div>
         <InputNoControlado></InputNoControlado>        
         <InputNoControladoForm></InputNoControladoForm>
         <InputControlado></InputControlado>        
+        <InputSelectOption></InputSelectOption>      
     </div>
 )
 export default App
