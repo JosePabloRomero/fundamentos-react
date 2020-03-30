18d12b16ad88f3dba2b4932fffe6b01f44ab8d7a
Jose Pablo Romero <josepablo.developer@gmail.com>
2020-03-30 17:32:20 -0500

    Componentes de cliclos de vida. Se inicio con componentDidMout y componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import InputNoControladoForm from './components/InputNoControladoForm'
 import InputControlado from './components/InputControlado'
 import InputSelectOption from './components/InputSelectOption' */
 //import LlamadaAPI from './components/LlamadaAPI'
-import BuscadorPeliculas from './components/BuscadorPeliculas'
+//import BuscadorPeliculas from './components/BuscadorPeliculas'
+import LifeCicle from './components/LifeClicle'
 const App = () => (
     <div>
         {/* {<InputNoControlado></InputNoControlado>        
         <InputNoControladoForm></InputNoControladoForm>
         <InputControlado></InputControlado>        
         <InputSelectOption></InputSelectOption>     
-        <LlamadaAPI/>
-        */}  
+        <LlamadaAPI/>        
         <BuscadorPeliculas/>
+        */}
+        <LifeCicle></LifeCicle>
     </div>
 )
 export default App
